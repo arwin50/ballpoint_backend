1f5c4a7eb5b2a705a0e94bdb93e2e9dc16311bea
Loweso <dlouisefermin@gmail.com>
2025-04-03 00:40:15 +0800

    search function but only backend

--- notes/urls.py
+++ notes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import NoteDocumentListCreate, NoteDocumentDetail
 
 urlpatterns = [
-    path('notes', NoteDocumentListCreate.as_view(), name='note-list'),
+    path('notes/', NoteDocumentListCreate.as_view(), name='note-list'),
     path('notes/<int:pk>', NoteDocumentDetail.as_view(), name='note-detail'),
 ]
