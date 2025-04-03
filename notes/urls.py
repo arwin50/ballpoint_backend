4061c9b03d7e0b9bf95211877b15ce441c5cfbde
Loweso <dlouisefermin@gmail.com>
2025-04-03 23:59:59 +0800

    category CRUD backend

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import NoteDocumentListCreate, NoteDocumentDetail
+from .views import *
 
 urlpatterns = [
     path('', NoteDocumentListCreate.as_view(), name='note-list'),
-    path('/<int:pk>', NoteDocumentDetail.as_view(), name='note-detail'),
+    path('<int:pk>', NoteDocumentDetail.as_view(), name='note-detail'),
+    path("categories/", CategoryListCreate.as_view(), name="category-list"),
+    path("categories/<str:label>/", CategoryDetail.as_view(), name="category-detail"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,7 @@
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
-from .models import NoteDocument
+from .models import NoteDocument, Category
 from rest_framework import generics
-from .serializers import NoteDocumentSerializer
+from .serializers import NoteDocumentSerializer, CategorySerializer
 from django.db.models import Q
 
 class NoteDocumentListCreate(generics.ListCreateAPIView):
@@ -23,3 +23,12 @@ class NoteDocumentListCreate(generics.ListCreateAPIView):
 class NoteDocumentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = NoteDocument.objects.all()
     serializer_class = NoteDocumentSerializer
+
+class CategoryListCreate(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = "label"
