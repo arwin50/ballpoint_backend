a2bb67076811364ff8c7bad9645586f7046ea93e
Bettina Ligero <ligerobc24@gmail.com>
2025-05-06 00:24:48 +0800

    AI Organize

--- aiextract/urls.py
+++ aiextract/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import extract_text,summarize_text
+from .views import extract_text,summarize_text, organize_text
 
 urlpatterns = [
     path("extract-text", extract_text, name="extract_text"),
-    path("summarize-text",summarize_text, name="summarize_text")
+    path("summarize-text",summarize_text, name="summarize_text"),
+    path("organize-text/", organize_text, name='organize_text'),
+
 ]
