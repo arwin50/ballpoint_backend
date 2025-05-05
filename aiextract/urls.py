1f2dcf1f7ad969a5baeaec6fc09feaa3fd695e06
Loweso <dlouisefermin@gmail.com>
2025-05-05 08:51:19 +0800

    refresh

--- aiextract/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import extract_text, WhisperTranscribeView
-
-urlpatterns = [
-    path("extract-text", extract_text, name="extract_text"),
-    path("whisper-audio", WhisperTranscribeView.as_view(), name="whisper-transcribe"),
-]
